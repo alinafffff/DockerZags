9e7882ef799cd9ab07a34899bcabf05c1bd8ad28
unknown <alfasar1@yandex.ru>
2024-01-13 22:10:19 +0400

    delete all in Scripts and add zagsSql.sql and change D-C.yaml

---
+++ scripts/zagsSql.sql
@@ -0,0 +1,50 @@
+CREATE TABLE certificates(
+    id serial PRIMARY KEY,
+    status varchar(50) NOT NULL,
+    date date NOT NULL,
+	act_number int NOT NULL,
+    zags varchar(50) NOT NULL,
+    id_person int NOT NULL,
+	id_facilities int NOT NULL,
+	checking int NOT NULL,
+	FOREIGN KEY (id_person) REFERENCES person(id),
+	FOREIGN KEY (id_facilities) REFERENCES facilities(id)
+);
+
+CREATE TABLE facilities(
+	id serial PRIMARY KEY,
+	type varchar(50) NOT NULL,
+	tariff int NOT NULL
+);
+
+CREATE TABLE person(
+	id serial PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	surname varchar(50) NOT NULL,
+	patronymic varchar(50) NOT NULL,
+	birthday date NOT NULL,
+	passport_series int NOT NULL,
+	passport_number int NOT NULL,
+	snils varchar(50) NOT NULL,
+	email varchar(50) NOT NULL
+);
+
+CREATE TABLE role(
+	id serial PRIMARY KEY,
+	role varchar(50) NOT NULL,
+	login varchar(50) NOT NULL,
+	password varchar(50) NOT NULL
+);
+
+INSERT INTO role(role, login, password)
+VALUES('admin', 'alina', 12345678);
+
+INSERT INTO facilities(type, tariff)
+VALUES('Регистрация брака', 350),
+('Регистрация расторжения брака', 350),
+('Регистрация рождения', 250),
+('Регистрация смерти', 250),
+('Регистрация смены имени', 450),
+('Регистрация установления отцовства', 250),
+('Регистрация усыновления/удочерения', 250);
+
