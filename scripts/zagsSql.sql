492d2f7003856a4087962f8dcb15daa3bce6f3b9
unknown <alfasar1@yandex.ru>
2024-01-14 15:07:21 +0400

    sql again not add

--- scripts/zagsSql.sql
+++ scripts/zagsSql.sql
@@ -37,63 +37,63 @@
 --	password varchar(50) NOT NULL
 --);
 
-
-DROP TABLE IF EXISTS certificates;
-DROP TABLE IF EXISTS facilities;
-DROP TABLE IF EXISTS person;
-DROP TABLE IF EXISTS role;
-
-CREATE TABLE IF NOT EXISTS public.role
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    role character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    login character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    password character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT role_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS public.facilities
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    type character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    tariff integer NOT NULL,
-    CONSTRAINT facilities_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS public.person
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    surname character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    patronymic character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    birthday date NOT NULL,
-    passport_series integer NOT NULL,
-    passport_number integer NOT NULL,
-    snils character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT person_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS public.certificates
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    status character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    data date NOT NULL,
-    act_number integer NOT NULL,
-    zags character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    id_person integer NOT NULL,
-    id_facilities integer NOT NULL,
-    checking integer NOT NULL,
-    CONSTRAINT certificates_pkey PRIMARY KEY (id),
-    CONSTRAINT certificates_id_facilities_fkey FOREIGN KEY (id_facilities)
-        REFERENCES public.facilities (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT certificates_id_person_fkey FOREIGN KEY (id_person)
-        REFERENCES public.person (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
+--
+--DROP TABLE IF EXISTS certificates;
+--DROP TABLE IF EXISTS facilities;
+--DROP TABLE IF EXISTS person;
+--DROP TABLE IF EXISTS role;
+--
+--CREATE TABLE IF NOT EXISTS public.role
+--(
+--    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--    role character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    login character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    password character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    CONSTRAINT role_pkey PRIMARY KEY (id)
+--);
+--
+--CREATE TABLE IF NOT EXISTS public.facilities
+--(
+--    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--    type character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    tariff integer NOT NULL,
+--    CONSTRAINT facilities_pkey PRIMARY KEY (id)
+--);
+--
+--CREATE TABLE IF NOT EXISTS public.person
+--(
+--    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    surname character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    patronymic character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    birthday date NOT NULL,
+--    passport_series integer NOT NULL,
+--    passport_number integer NOT NULL,
+--    snils character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    CONSTRAINT person_pkey PRIMARY KEY (id)
+--);
+--
+--CREATE TABLE IF NOT EXISTS public.certificates
+--(
+--    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--    status character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    data date NOT NULL,
+--    act_number integer NOT NULL,
+--    zags character varying(50) COLLATE pg_catalog."default" NOT NULL,
+--    id_person integer NOT NULL,
+--    id_facilities integer NOT NULL,
+--    checking integer NOT NULL,
+--    CONSTRAINT certificates_pkey PRIMARY KEY (id),
+--    CONSTRAINT certificates_id_facilities_fkey FOREIGN KEY (id_facilities)
+--        REFERENCES public.facilities (id) MATCH SIMPLE
+--        ON UPDATE NO ACTION
+--        ON DELETE NO ACTION,
+--    CONSTRAINT certificates_id_person_fkey FOREIGN KEY (id_person)
+--        REFERENCES public.person (id) MATCH SIMPLE
+--        ON UPDATE NO ACTION
+--        ON DELETE NO ACTION
+--);
 
 INSERT INTO role(role, login, password)
 VALUES('admin', 'alina', 12345678);
