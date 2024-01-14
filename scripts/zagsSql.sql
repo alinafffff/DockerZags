2343d81a1cbc31cfac8c91bed6fb1c7b51f95369
unknown <alfasar1@yandex.ru>
2024-01-14 15:20:32 +0400

    pls work

--- scripts/zagsSql.sql
+++ scripts/zagsSql.sql
@@ -1,109 +1,298 @@
---CREATE TABLE person(
---	id se,
---	name varchar(50) NOT NULL,
---	surname varchar(50) NOT NULL,
---	patronymic varchar(50) NOT NULL,
---	birthday date NOT NULL,
---	passport_series int NOT NULL,
---	passport_number int NOT NULL,
---	snils varchar(50) NOT NULL,
---	email varchar(50) NOT NULL
---);
 --
---CREATE TABLE facilities(
---	id int not null generated always as identity (increment by 1),
---	type varchar(50) NOT NULL,
---	tariff int NOT NULL
---);
+-- PostgreSQL database dump
 --
---CREATE TABLE certificates(
---    id int not null generated always as identity (increment by 1),
---    status varchar(50) NOT NULL,
---    data date NOT NULL,
---	act_number int NOT NULL,
---    zags varchar(50) NOT NULL,
---    id_person int NOT NULL,
---	id_facilities int NOT NULL,
---	checking int NOT NULL,
---	FOREIGN KEY (id_person) REFERENCES person(id),
---	FOREIGN KEY (id_facilities) REFERENCES facilities(id)
---);
---
---
---CREATE TABLE role(
---	id int not null generated always as identity (increment by 1),
---	role varchar(50) NOT NULL,
---	login varchar(50) NOT NULL,
---	password varchar(50) NOT NULL
---);
+
+-- Dumped from database version 15.3
+-- Dumped by pg_dump version 15.3
+
+-- Started on 2024-01-14 14:50:47
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
 
 --
---DROP TABLE IF EXISTS certificates;
---DROP TABLE IF EXISTS facilities;
---DROP TABLE IF EXISTS person;
---DROP TABLE IF EXISTS role;
+-- TOC entry 219 (class 1259 OID 33616)
+-- Name: certificates; Type: TABLE; Schema: public; Owner: postgres
 --
---CREATE TABLE IF NOT EXISTS public.role
---(
---    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
---    role character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    login character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    password character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    CONSTRAINT role_pkey PRIMARY KEY (id)
---);
---
---CREATE TABLE IF NOT EXISTS public.facilities
---(
---    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
---    type character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    tariff integer NOT NULL,
---    CONSTRAINT facilities_pkey PRIMARY KEY (id)
---);
---
---CREATE TABLE IF NOT EXISTS public.person
---(
---    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
---    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    surname character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    patronymic character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    birthday date NOT NULL,
---    passport_series integer NOT NULL,
---    passport_number integer NOT NULL,
---    snils character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    CONSTRAINT person_pkey PRIMARY KEY (id)
---);
---
---CREATE TABLE IF NOT EXISTS public.certificates
---(
---    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
---    status character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    data date NOT NULL,
---    act_number integer NOT NULL,
---    zags character varying(50) COLLATE pg_catalog."default" NOT NULL,
---    id_person integer NOT NULL,
---    id_facilities integer NOT NULL,
---    checking integer NOT NULL,
---    CONSTRAINT certificates_pkey PRIMARY KEY (id),
---    CONSTRAINT certificates_id_facilities_fkey FOREIGN KEY (id_facilities)
---        REFERENCES public.facilities (id) MATCH SIMPLE
---        ON UPDATE NO ACTION
---        ON DELETE NO ACTION,
---    CONSTRAINT certificates_id_person_fkey FOREIGN KEY (id_person)
---        REFERENCES public.person (id) MATCH SIMPLE
---        ON UPDATE NO ACTION
---        ON DELETE NO ACTION
---);
 
-INSERT INTO role(role, login, password)
-VALUES('admin', 'alina', 12345678);
+CREATE TABLE public.certificates (
+    id integer NOT NULL,
+    status character varying(50) NOT NULL,
+    data date NOT NULL,
+    act_number integer NOT NULL,
+    zags character varying(50) NOT NULL,
+    id_person integer NOT NULL,
+    id_facilities integer NOT NULL,
+    checking integer NOT NULL
+);
 
-INSERT INTO facilities(type, tariff)
-VALUES('Регистрация брака', 350),
-('Регистрация расторжения брака', 350),
-('Регистрация рождения', 250),
-('Регистрация смерти', 250),
-('Регистрация смены имени', 450),
-('Регистрация установления отцовства', 250),
-('Регистрация усыновления/удочерения', 250);
+
+ALTER TABLE public.certificates OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 33615)
+-- Name: certificates_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.certificates ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.certificates_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 217 (class 1259 OID 33602)
+-- Name: facilities; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.facilities (
+    id integer NOT NULL,
+    type character varying(50) NOT NULL,
+    tariff integer NOT NULL
+);
+
+
+ALTER TABLE public.facilities OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 33601)
+-- Name: facilities_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.facilities ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.facilities_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 215 (class 1259 OID 33590)
+-- Name: person; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.person (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    surname character varying(50) NOT NULL,
+    patronymic character varying(50) NOT NULL,
+    birthday date NOT NULL,
+    passport_series integer NOT NULL,
+    passport_number integer NOT NULL,
+    snils character varying(50) NOT NULL,
+    email character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.person OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 33589)
+-- Name: person_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.person ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.person_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 221 (class 1259 OID 33630)
+-- Name: role; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.role (
+    id integer NOT NULL,
+    role character varying(50) NOT NULL,
+    login character varying(50) NOT NULL,
+    password character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.role OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 33629)
+-- Name: role_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.role ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.role_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 3345 (class 0 OID 33616)
+-- Dependencies: 219
+-- Data for Name: certificates; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.certificates (id, status, data, act_number, zags, id_person, id_facilities, checking) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3343 (class 0 OID 33602)
+-- Dependencies: 217
+-- Data for Name: facilities; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.facilities (id, type, tariff) FROM stdin;
+1	??????????? ?????	350
+2	??????????? ??????????? ?????	350
+3	??????????? ????????	250
+4	??????????? ??????	250
+5	??????????? ????? ?????	450
+6	??????????? ???????????? ?????????	250
+7	??????????? ???????????/??????????	250
+\.
+
+
+--
+-- TOC entry 3341 (class 0 OID 33590)
+-- Dependencies: 215
+-- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.person (id, name, surname, patronymic, birthday, passport_series, passport_number, snils, email) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3347 (class 0 OID 33630)
+-- Dependencies: 221
+-- Data for Name: role; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.role (id, role, login, password) FROM stdin;
+1	admin	alina	12345678
+\.
+
+
+--
+-- TOC entry 3353 (class 0 OID 0)
+-- Dependencies: 218
+-- Name: certificates_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.certificates_id_seq', 1, false);
+
+
+--
+-- TOC entry 3354 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: facilities_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.facilities_id_seq', 7, true);
+
+
+--
+-- TOC entry 3355 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: person_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.person_id_seq', 1, false);
+
+
+--
+-- TOC entry 3356 (class 0 OID 0)
+-- Dependencies: 220
+-- Name: role_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.role_id_seq', 1, true);
+
+
+--
+-- TOC entry 3193 (class 2606 OID 33634)
+-- Name: certificates certificates_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.certificates
+    ADD CONSTRAINT certificates_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3191 (class 2606 OID 33612)
+-- Name: facilities facilities_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.facilities
+    ADD CONSTRAINT facilities_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3189 (class 2606 OID 33614)
+-- Name: person person_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT person_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3195 (class 2606 OID 33636)
+-- Name: role role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role
+    ADD CONSTRAINT role_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3196 (class 2606 OID 33624)
+-- Name: certificates certificates_id_facilities_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.certificates
+    ADD CONSTRAINT certificates_id_facilities_fkey FOREIGN KEY (id_facilities) REFERENCES public.facilities(id);
+
+
+--
+-- TOC entry 3197 (class 2606 OID 33619)
+-- Name: certificates certificates_id_person_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.certificates
+    ADD CONSTRAINT certificates_id_person_fkey FOREIGN KEY (id_person) REFERENCES public.person(id);
+
+
+-- Completed on 2024-01-14 14:50:47
+
+--
+-- PostgreSQL database dump complete
+--
+
+
 
