5044ebc55fe545ea9f6edf3d01b07fb8eda0aafd
unknown <alfasar1@yandex.ru>
2024-01-14 10:21:25 +0400

    id int not null generated always as identity (increment by 1),

--- scripts/zagsSql.sql
+++ scripts/zagsSql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE certificates(
-    id serial PRIMARY KEY,
+    id int not null generated always as identity (increment by 1),
     status varchar(50) NOT NULL,
     data date NOT NULL,
 	act_number int NOT NULL,
@@ -12,13 +12,13 @@ CREATE TABLE certificates(
 );
 
 CREATE TABLE facilities(
-	id serial PRIMARY KEY,
+	id int not null generated always as identity (increment by 1),
 	type varchar(50) NOT NULL,
 	tariff int NOT NULL
 );
 
 CREATE TABLE person(
-	id serial PRIMARY KEY,
+	id int not null generated always as identity (increment by 1),
 	name varchar(50) NOT NULL,
 	surname varchar(50) NOT NULL,
 	patronymic varchar(50) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE person(
 );
 
 CREATE TABLE role(
-	id serial PRIMARY KEY,
+	id int not null generated always as identity (increment by 1),
 	role varchar(50) NOT NULL,
 	login varchar(50) NOT NULL,
 	password varchar(50) NOT NULL
