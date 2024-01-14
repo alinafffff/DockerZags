4ce2148f2121845060f9aef10c4a40e3a5b3d028
unknown <alfasar1@yandex.ru>
2024-01-14 13:28:01 +0400

    changes in sql comments all

--- scripts/zagsSql.sql
+++ scripts/zagsSql.sql
@@ -1,41 +1,41 @@
-CREATE TABLE person(
-	id int not null generated always as identity (increment by 1),
-	name varchar(50) NOT NULL,
-	surname varchar(50) NOT NULL,
-	patronymic varchar(50) NOT NULL,
-	birthday date NOT NULL,
-	passport_series int NOT NULL,
-	passport_number int NOT NULL,
-	snils varchar(50) NOT NULL,
-	email varchar(50) NOT NULL
-);
-
-CREATE TABLE facilities(
-	id int not null generated always as identity (increment by 1),
-	type varchar(50) NOT NULL,
-	tariff int NOT NULL
-);
-
-CREATE TABLE certificates(
-    id int not null generated always as identity (increment by 1),
-    status varchar(50) NOT NULL,
-    data date NOT NULL,
-	act_number int NOT NULL,
-    zags varchar(50) NOT NULL,
-    id_person int NOT NULL,
-	id_facilities int NOT NULL,
-	checking int NOT NULL,
-	FOREIGN KEY (id_person) REFERENCES person(id),
-	FOREIGN KEY (id_facilities) REFERENCES facilities(id)
-);
-
-
-CREATE TABLE role(
-	id int not null generated always as identity (increment by 1),
-	role varchar(50) NOT NULL,
-	login varchar(50) NOT NULL,
-	password varchar(50) NOT NULL
-);
+--CREATE TABLE person(
+--	id int not null generated always as identity (increment by 1),
+--	name varchar(50) NOT NULL,
+--	surname varchar(50) NOT NULL,
+--	patronymic varchar(50) NOT NULL,
+--	birthday date NOT NULL,
+--	passport_series int NOT NULL,
+--	passport_number int NOT NULL,
+--	snils varchar(50) NOT NULL,
+--	email varchar(50) NOT NULL
+--);
+--
+--CREATE TABLE facilities(
+--	id int not null generated always as identity (increment by 1),
+--	type varchar(50) NOT NULL,
+--	tariff int NOT NULL
+--);
+--
+--CREATE TABLE certificates(
+--    id int not null generated always as identity (increment by 1),
+--    status varchar(50) NOT NULL,
+--    data date NOT NULL,
+--	act_number int NOT NULL,
+--    zags varchar(50) NOT NULL,
+--    id_person int NOT NULL,
+--	id_facilities int NOT NULL,
+--	checking int NOT NULL,
+--	FOREIGN KEY (id_person) REFERENCES person(id),
+--	FOREIGN KEY (id_facilities) REFERENCES facilities(id)
+--);
+--
+--
+--CREATE TABLE role(
+--	id int not null generated always as identity (increment by 1),
+--	role varchar(50) NOT NULL,
+--	login varchar(50) NOT NULL,
+--	password varchar(50) NOT NULL
+--);
 
 INSERT INTO role(role, login, password)
 VALUES('admin', 'alina', 12345678);
